b899e75da124e93e034ab7d5c21154bdc654ef73
Luciano Pereira Soares <lpsoares@gmail.com>
2022-10-10 22:42:04 -0300

    aula 12

---
+++ material/aulas/12-paralelismo/exemplos/exemplo1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <omp.h>
+#include <unistd.h>
+
+
+int main() {
+
+    // Define o número máximo de threads
+    omp_set_num_threads(4);
+
+    // Executa código em paralelo
+    #pragma omp parallel
+    {
+        #pragma omp for
+        for(int i = 0; i < 30; i++) {
+            usleep(10 * omp_get_thread_num());
+            std::cout << "Hello World (" << i << "," << omp_get_thread_num() << ")" << std::endl;
+        }
+    }
+
+    return 0;
+
+}
